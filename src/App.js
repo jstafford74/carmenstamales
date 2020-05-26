100998547a01e19f6d0efc1d08b7bb4fac896fb3
Jeremiah Stafford <52318268+jstafford74@users.noreply.github.com>
2020-05-26 05:59:05 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import Dashboard from './components/Dashboard/Dashboard';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
